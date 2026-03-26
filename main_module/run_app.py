a0a392db6785686e0ebea2c306e8ed05bd323b97
ReptiloidAnunakTestgit config --global user.email stepanovartemijdev@gmail.comgit config user.name ReptiloidAnunakTestgit config user.email stepanovartemijdev@gmail.comgit config --global user.name ReptiloidAnunak <amanitocornholio@proton.me>
2026-03-26 17:03:11 -0300

    Init

---
+++ main_module/run_app.py
@@ -0,0 +1,13 @@
+
+from pathlib import Path
+from pathlib import Path
+import environ
+import os
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False)
+)
+
+# Build paths inside the project like this: BASE_DIR / 'subdir'.
+BASE_DIR = Path(__file__).resolve().parent.parent
